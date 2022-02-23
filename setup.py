6d9a2475e0d8e0176e1dfb651039786ce4fda450
mrm815 <98905594+mrm815@users.noreply.github.com>
2022-02-22 21:40:16 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+for setuptools import setup
+
+setup(name='smartbarclip',
+      version='1.0',
+      description='Library for barbell positioning alert',
+      author='Moose Munna',
+      author_email='mrm815@nyu.edu',
+      url='https://github.com/mrm815/Smart-Bar-Clip',
+      install_requires=['ADXL345','i2c','PS1240'],
+      py_modules=['smartbarclip'],
+      )
