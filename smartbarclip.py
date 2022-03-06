4c567fb9154694f847d7a84e7785d229a25ff711
mrm815 <98905594+mrm815@users.noreply.github.com>
2022-03-06 15:04:59 -0500

    Update smartbarclip.py

--- smartbarclip.py
+++ smartbarclip.py
@@ -1,6 +1,11 @@
 import random
-Gyro_Address=0x53
+import board
+import busio
+import adafruit_adxl34x
 
+Gyro_Address=0x53
+i2c = board.I2C()
+gyro = adafruit_adxl34x.ADXL345(i2c)
 
 def setup():
     print("All peripherals and sensors have been set up")
@@ -14,3 +19,11 @@ def buzzer_pulse():
 
 def gyro_x():
     return random.randint(0,100)
+
+try:
+    while True:
+        print(gyro.acceleration)
+        time.sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    sys.exit()
