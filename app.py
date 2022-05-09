e840c77de41aa307efc975f17c4b6b475b11fe95
mrm815 <98905594+mrm815@users.noreply.github.com>
2022-05-09 13:31:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import smartbarclip as sbc
 import time
 
+
 sbc.setup()
 
 x_range=4
 y_range=4
 
 def dlift():
-  if (((-x_range)<sbc.gyro_x()<x_range) or ((-y_range)<sbc.gyro_y()<y_range)):
-    print("Form error")
+  if ((-x_range)<sbc.gyro_x()<x_range):
+    print(" X Form error")
+    sbc.buzzer_long()
+  if ((-y_range)<sbc.gyro_y()<y_range):
+    print("Y Form Error")
+    sbc.buzzer_pulse()
     
 try:
   while True:
     #print(sbc.gyro_x(),sbc.gyro_y(),sbc.gyro_z())
     #time.sleep(1)
     dlift()
+    time.sleep(0.5)
 except KeyboardInterrupt:
   GPIO.cleanup()
   sys.exit()
