c6b54b8b98c0d53ab5fb6c1863b7aed228230115
mrm815 <98905594+mrm815@users.noreply.github.com>
2022-05-09 12:48:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import smartbarclip as sbc
 
 sbc.setup()
+
+x_range=20
+y_range=20
+z_range= 20
+
+def dlift():
+  if (((-z_range)<sbc.gyro_z()<z_range) or ((-y_range)<sbc.gyro_y()<y_range)):
+    print("Form error")
+    
+try:
+  while True:
+    gyro_x()
+    gyro_y()
+    gyro_z()
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  sys.exit()
