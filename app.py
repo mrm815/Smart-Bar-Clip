45e91aaa234d75d04c0245188819fbd076279238
mrm815 <98905594+mrm815@users.noreply.github.com>
2022-05-09 19:03:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import smartbarclip as sbc
 import time
 import RPi.GPIO as GPIO
 import sys
+from flask import Flask, render_template, redirect, request
 
 
 sbc.setup()
@@ -16,7 +17,22 @@ def dlift():
   if (not((-y_range)<sbc.gyro_y()<y_range)):
     print("Y Form Error")
     sbc.buzzer_pulse()
-    
+
+app= Flask(__name__, static_folder='assets')
+
+@app.route("/")
+def home():
+  return redirect("/templates/index")
+
+@app.route("templates/index")
+def home_template():
+  return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',port=80,debug=True, threaded= True)
+
 try:
   while True:
     #print(sbc.gyro_x(),sbc.gyro_y(),sbc.gyro_z())
